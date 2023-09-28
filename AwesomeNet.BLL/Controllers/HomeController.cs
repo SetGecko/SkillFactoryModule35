570b1314a1f85460e94f1711b7080ac31735c17f
Ипполитов Алексей <81488631+SetGecko@users.noreply.github.com>
2023-09-28 18:16:48 +0300

    Add files via upload

---
+++ AwesomeNet.BLL/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+
+using AwesomeNet.Repository.Models;
+using AwesomeNet.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace AwesomeNet.CNTR.Controllers
+{
+    //[Route("[controller]")] //только для теста
+    //[ApiController] //только для теста
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMapper _mapper;
+        private readonly SignInManager<User> _signInManager;
+ 
+
+        public HomeController(ILogger<HomeController> logger,SignInManager<User> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+
+        }
+
+        /// <summary>
+        /// только для теста
+        /// </summary>
+        [Route("Check_status")]
+        public IActionResult CheckStatus()
+        {
+            return Ok("Hello!");
+        }
+
+        [Route("")]
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+                return View(new MainViewModel());
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AwesomeNet.Unit35/Startup.cs
@@ -0,0 +1,85 @@
+using AutoMapper;
+using AwesomeNet.ViewModels;
+using AwesomeNet.Repository.Data;
+using AwesomeNet.Repository.Data.Repository;
+using AwesomeNet.Repository.Models;
+using AwesomeNet.Repository.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace AwesomeNet.Unit35
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            var mapperConfig = new MapperConfiguration((v) =>
+            {
+                v.AddProfile(new MappingProfile());
+            });
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services
+                .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection))
+                .AddUnitOfWork()
+                .AddCustomRepository<Friend, FriendsRepository>()
+                .AddCustomRepository<Message, MessageRepository>()
+                .AddIdentity<User, IdentityRole>(opts =>
+                {
+                    opts.Password.RequiredLength = 5;
+                    opts.Password.RequireNonAlphanumeric = false;
+                    opts.Password.RequireLowercase = false;
+                    opts.Password.RequireUppercase = false;
+                    opts.Password.RequireDigit = false;
+                })
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
